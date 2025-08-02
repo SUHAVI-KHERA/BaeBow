255575c66c6df190900a5edbfea7dacc108df7c6
Suhavi Khera <suhavikhera202@gmail.com>
2025-08-02 08:36:53 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -42,11 +42,13 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
+    // Clean up props to prevent passing invalid attributes to the DOM
+    const { fdprocessedid, ...rest } = props as any;
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
-        {...props}
+        {...rest}
       />
     )
   }
